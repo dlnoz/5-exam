c271f6489c8e2e337740523196d4249a9621a657
Dilnozass <rakhma1ullaevaa@gmail.com>
2025-07-16 20:48:55 +0500

    push

--- src/components/Butttons.jsx
+++ src/components/Butttons.jsx
@@ -4,9 +4,8 @@ function Buttons({title, extraClass, Img}){
         <button className={`${extraClass} w-[266px] h-[69px] rounded-[10px] flex justify-center items-center gap-[10px]`}>
       {
         Img ? <img src={Img ||null} alt="Footer Btn Img" /> : ""
-      }
-            
-             {title}
+      }    
+      {title}
         </button>
     )
 }
--- src/modules/GetStart/index.jsx
+++ src/modules/GetStart/index.jsx
@@ -9,7 +9,7 @@ function GetStart(){
                 <div data-aos="zoom-out-right" className="mt-[120px] flex flex-col gap-[12px]">
                     <h1 className="font-normal text-[#242F65] text-[40px]">Ready to get start with UIUXer?</h1>
                     <p className="font-normal text-[#525665] text-[20px]">Well-planned layouts with a clear structure that can be utilized to demonstrate your inventiveness in the digital business field.</p>
-                    <Buttons extraClass="bg-[#FF774C] w-[274px] h-[69px] font-normal text-white text-[18px] mt-[18px]" title={"Get Start Now"}/>
+                    <Buttons extraClass="bg-[#FF774C] w-[274px] h-[69px] font-normal text-white text-[18px] mt-[18px] hover:bg-transparent hover:border-[#FF774C] hover:border-[2px] hover:text-[#FF774C] duration-300" title={"Get Start Now"}/>
                 </div>
                 <div className="mt-[39px]">
                     <img data-aos="zoom-out-left" src={GetReadyImg} alt="Hero Img" width={1075} height={463}/>
